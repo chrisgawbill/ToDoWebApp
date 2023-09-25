fdd67c764f16efc1ca19839b71f052ed88450d03
Chris Gawbill <cgawbill@protonmail.com>
2023-09-25 16:43:27 -0500

    Started constructing the skeleton

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import { useState} from "react";
+
+function Home(){
+    let [todo, setToDo]= useState([])
+    return(
+        <div>
+            for(task in todo){
+                
+            }
+        </div>
+    );
+}
+export default Home;
