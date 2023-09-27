7274d42b11219a79687ead1e1802a93fd79f024d
= <cgawbill@protonmail.com>
2023-09-27 10:55:07 -0500

    Converted name in panel from h2 to text

--- src/components/add-todo-panel.tsx
+++ src/components/add-todo-panel.tsx
@@ -1,24 +1,33 @@
-import { Col, Row } from "react-bootstrap"
-import { Todo } from "../data/Todo"
+import { Col, Row } from "react-bootstrap";
+import { Todo } from "../data/Todo";
+import { useState } from "react";
 
-interface AddToDoPanelProps{
-    toDoItem:Todo
+interface AddToDoPanelProps {
+  toDoItem: Todo;
 }
-export default function AddToDoPanel({toDoItem}: AddToDoPanelProps){
- return(
+export default function AddToDoPanel({ toDoItem }: AddToDoPanelProps) {
+  const [toDoItemName, setToDoItemName] = useState<string>(toDoItem.name);
+  return (
     <Col>
-        <Row>
-           <h2>{toDoItem.name}</h2> 
-        </Row>
-        <Row>
-            <p>Status: </p>{ConvertStatusBool(toDoItem.completed)}
-        </Row>
+      <Row>
+        <h2>
+          <input
+            type="text"
+            value={toDoItemName}
+            style={{ border: "none", width: "100%" }}
+          />
+        </h2>
+      </Row>
+      <Row>
+        <p>Status: </p>
+        {ConvertStatusBool(toDoItem.completed)}
+      </Row>
     </Col>
- )   
+  );
 }
-function ConvertStatusBool(toDoItemStatus:Boolean){
-    if(toDoItemStatus === true){
-        return "Completed"
-    }
-    return "Not Completed"
+function ConvertStatusBool(toDoItemStatus: Boolean) {
+  if (toDoItemStatus === true) {
+    return "Completed";
+  }
+  return "Not Completed";
 }
